173a59a52600d4dff549495bc2efee423db50fd1
Fabian Streitel <streitel@cqse.eu>
2020-03-02 16:55:58 +0100

    initial commit of simple Gradle/Java/JUnit 5 project with unit tests

---
+++ README.md
@@ -0,0 +1,5 @@
+# Test Impact Analysis Tutorial
+
+This repository is used in our TIA tutorial. For more info, please refer to 
+[the Teamscale userguide](https://docs.teamscale.com/tutorial/tia-java/).
+
---
+++ src/main/java/tia/Calculator.java
@@ -0,0 +1,18 @@
+package tia;
+
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return sumImpl(a, b);
+    }
+
+    private int sumImpl(int a, int b) {
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        System.out.println("gi");
+        return a - b;
+    }
+
+}
